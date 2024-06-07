bcc5bb56bc8ead63d511ff18af87d6fb1a17c2c6
Akhil Rajan <rajanakhil07@gmail.com>
2024-06-07 14:58:26 +0530

    added passenger model as joined table

--- src/main/java/com/akhil/UberReviewService/models/PassengerReview.java
+++ src/main/java/com/akhil/UberReviewService/models/PassengerReview.java
@@ -1,5 +1,6 @@
 package com.akhil.UberReviewService.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.PrimaryKeyJoinColumn;
 import lombok.Getter;
@@ -11,5 +12,9 @@ import lombok.Setter;
 @PrimaryKeyJoinColumn(name = "passengerReviewId")
 public class PassengerReview extends Review{
 
+    @Column(nullable = false)
     private String passengerReviewContent;
+
+    @Column(nullable = false)
+    private String passengerRating;
 }
